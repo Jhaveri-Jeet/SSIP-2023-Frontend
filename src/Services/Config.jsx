e3bff01ff993092875d38804afff09dc5cc9dde3
JeetJhaveri <jeetwork941@gmail.com>
2023-11-22 18:54:36 +0530

    config file changed and token is been stored

--- src/Services/Config.jsx
+++ src/Services/Config.jsx
@@ -1,1 +1,7 @@
-export const prefixUrl = 'http://localhost:5000'
+import Cookies from "js-cookie";
+
+export const prefixUrl = "http://localhost:5000/";
+
+const accessToken = Cookies.get("access_token");
+
+export const tokenData = JSON.parse(atob(accessToken.split(".")[1]));
